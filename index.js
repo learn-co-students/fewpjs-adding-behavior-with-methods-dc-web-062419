d4a593bf3c76b79530d4302cc234a302ea23a748
github username <github email address>
2019-08-22 15:39:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+class Cat {
+   constructor(name, sex){
+       this.name = name;
+       this.sex = sex; 
+   }
+   speak(){
+       return `${this.name} says meow!`
+   }
+  }
+  
+  class Dog {
+    //...
+  }
+  
+  class Bird {
+    //...
+  }
